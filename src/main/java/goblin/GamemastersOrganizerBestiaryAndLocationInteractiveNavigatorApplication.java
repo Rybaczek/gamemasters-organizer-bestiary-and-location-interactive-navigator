ada98de21266fc67f52560980ec25496249083d1
Dawid Rybak <d.rybak.2001@gmail.com>
2024-06-05 17:49:46 +0200

    Initial commit

---
+++ src/main/java/goblin/GamemastersOrganizerBestiaryAndLocationInteractiveNavigatorApplication.java
@@ -0,0 +1,13 @@
+package goblin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamemastersOrganizerBestiaryAndLocationInteractiveNavigatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamemastersOrganizerBestiaryAndLocationInteractiveNavigatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/goblin/GamemastersOrganizerBestiaryAndLocationInteractiveNavigatorApplicationTests.java
@@ -0,0 +1,13 @@
+package goblin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamemastersOrganizerBestiaryAndLocationInteractiveNavigatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
